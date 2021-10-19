acf97c104d280e24efb247343bc0c9f4f87f0dd0
waghmareomkar <45145849+waghmareomkar@users.noreply.github.com>
2021-10-18 23:00:06 -0400

    train,test datasets

--- read_data.py
+++ read_data.py
@@ -5,7 +5,7 @@ import pickle
 
 def read_files(n1):
     label_path = "dataset/nasa_tropical_storm_competition_{}_labels/".format(n1)
-    source_path = "dataset/ nasa_tropical_storm_competition_{}_source/".format(n1)
+    source_path = "dataset/nasa_tropical_storm_competition_{}_source/".format(n1)
     data = list()
     for label_folder,source_folder in zip(os.listdir(label_path)[1:],os.listdir(source_path)[1:]):
         dict_ = dict()
@@ -15,6 +15,7 @@ def read_files(n1):
             dict_.update(json.load(fp))
         with open(source_path+source_folder+"/"+os.listdir(source_path+source_folder)[0],'rb') as fp:
             dict_.update(json.load(fp))
+        dict_['image_path'] = source_path+source_folder+"/"+os.listdir(source_path+source_folder)[1]
         data.append(dict_)
         
     return pd.DataFrame.from_dict(data, orient='columns')
@@ -22,6 +23,6 @@ def read_files(n1):
 train = read_files(n1= "train")
 test =  read_files(n1= "test")
 
-with open(".",'wb') as fp:
-    pickle.dump([fp,train,test],protocol = 4)
-    
+train.to_csv(r"outputs\train.csv", index = False)
+test.to_csv(r"outputs\test.csv", index = False)
+print("test")
